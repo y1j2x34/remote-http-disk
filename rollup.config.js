75cd47052f32b5f829d443b75bb87f9283967e9b
Jianxin Yang <y1j2x34@qq.com>
2022-09-05 21:52:56 +0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,2 @@
+require('sucrase/register/ts');
+module.exports = require('./build/rollup.config.ts').default;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function hello() {
+    return 'world';
+}
+interface A {}
+
+class B implements A {}
+new B();
